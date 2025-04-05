663a50c8a7ee32416aaea35e8c65f63752842fd0
Ryan Salisbury <rxs1035@student.bham.ac.uk>
2025-04-05 11:46:24 +0100

    added testing package and skeleton to backend

---
+++ Backend/src/controllers/BusinessController/BusinessController.test.ts
@@ -0,0 +1,7 @@
+import { describe, it } from "vitest";
+
+describe("BusinessController", () => {
+  describe("registerBusinessUser", () => {
+    it("runs tests to test registerBusinessUser controller function", () => {});
+  });
+});
